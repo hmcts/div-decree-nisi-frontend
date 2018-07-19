ada33b575c22e7bccf0114e5ff592ee41ef8c7ac
Dominic Taylor <dom.taylor7@gmail.com>
2018-07-19 15:59:28 +0100

    tests

--- test/e2e/pages/idam.step.js
+++ test/e2e/pages/idam.step.js
@@ -1,5 +1,5 @@
-const IdamLogin = require('mocks/steps/idamLogin/idamLogin.step');
-const content = require('mocks/steps/idamLogin/idamLogin.content');
+const IdamLogin = require('mocks/steps/idamLogin/IdamLogin.step');
+const content = require('mocks/steps/idamLogin/IdamLogin.content');
 
 function loginToIdam(success = true) {
   const I = this;
