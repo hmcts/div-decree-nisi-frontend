2fc111469ec9dee5b889f4e6d18b41d2862abc52
Dominic Taylor <dom.taylor7@gmail.com>
2018-07-25 18:35:31 +0100

    test

--- test/e2e/helpers/idamHelper.js
+++ test/e2e/helpers/idamHelper.js
@@ -5,48 +5,47 @@ const idamConfigHelper = require('./idamConfigHelper');
 
 const Helper = codecept_helper; // eslint-disable-line
 const args = idamConfigHelper.getArgs();
-const config = require('config');
 
 class IdamHelper extends Helper {
   _before() { // eslint-disable-line consistent-return
-    if (config.environment !== 'development') {
-      const randomString = randomstring.generate({
-        length: 16,
-        charset: 'numeric'
+    // if (config.environment !== 'development') {
+    const randomString = randomstring.generate({
+      length: 16,
+      charset: 'numeric'
+    });
+    const emailName = `simulate-delivered-${randomString}`;
+    const testEmail = `${emailName}@notifications.service.gov.uk`;
+    const testPassword = randomstring.generate(9);
+
+    args.testEmail = testEmail;
+    args.testPassword = testPassword;
+
+    idamConfigHelper.setTestEmail(testEmail);
+    idamConfigHelper.setTestPassword(testPassword);
+
+    return idamExpressTestHarness.createUser(args)
+      .then(() => {
+        logger.info(`Created IDAM test user: ${testEmail}`);
+        console.log('Created IDAM test user: ${testEmail}'); // eslint-disable-line
+      })
+      .catch(error => {
+        console.log('error creating idam user'); // eslint-disable-line
+        console.log(error); // eslint-disable-line
+        logger.warn(`Unable to create IDAM test user: ${error}`);
       });
-      const emailName = `simulate-delivered-${randomString}`;
-      const testEmail = `${emailName}@notifications.service.gov.uk`;
-      const testPassword = randomstring.generate(9);
-
-      args.testEmail = testEmail;
-      args.testPassword = testPassword;
-
-      idamConfigHelper.setTestEmail(testEmail);
-      idamConfigHelper.setTestPassword(testPassword);
-
-      return idamExpressTestHarness.createUser(args)
-        .then(() => {
-          logger.info(`Created IDAM test user: ${testEmail}`);
-          console.log('Created IDAM test user: ${testEmail}'); // eslint-disable-line
-        })
-        .catch(error => {
-          console.log('error creating idam user'); // eslint-disable-line
-          console.log(error); // eslint-disable-line
-          logger.warn(`Unable to create IDAM test user: ${error}`);
-        });
-    }
+    // }
   }
 
   _after() { // eslint-disable-line consistent-return
-    if (config.environment !== 'development') {
-      return idamExpressTestHarness.removeUser(args)
-        .then(() => {
-          logger.info(`Removed IDAM test user: ${args.testEmail}`);
-        })
-        .catch(error => {
-          logger.warn(`Unable to remove IDAM test user: ${error}`);
-        });
-    }
+    // if (config.environment !== 'development') {
+    return idamExpressTestHarness.removeUser(args)
+      .then(() => {
+        logger.info(`Removed IDAM test user: ${args.testEmail}`);
+      })
+      .catch(error => {
+        logger.warn(`Unable to remove IDAM test user: ${error}`);
+      });
+    // }
   }
 }
 
