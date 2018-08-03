c4c5826c775936314ff14ea1d94903ba9799bd68
Dominic Taylor <dom.taylor7@gmail.com>
2018-08-03 09:19:36 +0100

    add new decree nisi page

---
+++ steps/apply-for-decree-nisi/ApplyForDecreeNisi.step.js
@@ -0,0 +1,39 @@
+const { Question } = require('@hmcts/one-per-page/steps');
+const { form, text } = require('@hmcts/one-per-page/forms');
+const { goTo } = require('@hmcts/one-per-page/flow');
+const config = require('config');
+const idam = require('services/idam');
+const Joi = require('joi');
+const { getUserData } = require('middleware/ccd');
+
+class ApplyForDecreeNisi extends Question {
+  static get path() {
+    return config.paths.applyForDecreeNisi;
+  }
+
+  get session() {
+    return this.req.session;
+  }
+
+  get form() {
+    const answers = ['yes', 'no'];
+    const validAnswers = Joi.string()
+      .valid(answers)
+      .required();
+
+    const applyForDecreeNisi = text
+      .joi(this.content.errors.required, validAnswers);
+
+    return form({ applyForDecreeNisi });
+  }
+
+  next() {
+    return goTo(this.journey.steps.End);
+  }
+
+  get middleware() {
+    return [...super.middleware, idam.protect(), getUserData];
+  }
+}
+
+module.exports = ApplyForDecreeNisi;
