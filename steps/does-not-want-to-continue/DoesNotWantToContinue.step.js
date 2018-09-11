156c178bcf78305a4e94adf47fe09598b40685d8
John O'Neill <j.oneill@kainos.com>
2018-09-11 18:30:01 +0100

    DIV-2977: Got flow logic working

--- steps/does-not-want-to-continue/DoesNotWantToContinue.step.js
+++ steps/does-not-want-to-continue/DoesNotWantToContinue.step.js
@@ -1,16 +1,9 @@
-const { Page } = require('@hmcts/one-per-page');
+const { ExitPoint } = require('@hmcts/one-per-page');
 const config = require('config');
-const idam = require('services/idam');
 
-class DoesNotWantToContinue extends Page {
-  static get ignorePa11yWarnings() {
-    return ['WCAG2AA.Principle1.Guideline1_3.1_3_1.H48'];
-  }
+class DoesNotWantToContinue extends ExitPoint {
   static get path() {
     return config.paths.doesNotWantToContinue;
-  }
-  get middleware() {
-    return [...super.middleware, idam.setRedirectUri];
   }
 }
 
