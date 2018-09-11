cff09bccf3e9efd22f76ada0aaee7eeaf52d81fe
John O'Neill <j.oneill@kainos.com>
2018-09-11 11:39:34 +0100

    temp commit

--- steps/lived-apart-since-separation/LivedApartSinceSeparation.step.js
+++ steps/lived-apart-since-separation/LivedApartSinceSeparation.step.js
@@ -1,12 +1,13 @@
 /* eslint-disable max-len */
 const { Question } = require('@hmcts/one-per-page/steps');
-const { form, text } = require('@hmcts/one-per-page/forms');
 const { goTo } = require('@hmcts/one-per-page/flow');
 const config = require('config');
 const { answer } = require('@hmcts/one-per-page/checkYourAnswers');
 const idam = require('services/idam');
 const Joi = require('joi');
 const { getUserData } = require('middleware/ccd');
+
+const { form, text, errorFor, object } = require('@hmcts/one-per-page/forms');
 
 class LivedApartSinceSeparation extends Question {
   static get path() {
@@ -17,45 +18,64 @@ class LivedApartSinceSeparation extends Question {
     return this.req.session;
   }
 
+  /*
   get form() {
     const answers = ['yes', 'no'];
     const validAnswers = Joi.string()
       .valid(answers)
       .required();
 
-    const livedApartSinceSeparation = text
+    const hasLivedApartSinceSeparationAnswer = text
       .joi(this.content.errors.required, validAnswers);
 
-    return form({ livedApartSinceSeparation });
+    return form({ hasLivedApartSinceSeparationAnswer });
   }
-
-  /*
-  if no then display popup
    */
 
+  get form() {
+    const validateKillMe = ({ hasLivedApartSinceSeparationAnswer = '', livedApartSinceSeparation = '' }) => {
+      // only validate if user has answered hasLivedApartSinceSeparationAnswer
+      const hasntAnsweredQuestion = !hasLivedApartSinceSeparationAnswer.length;
+      if (hasntAnsweredQuestion) {
+        return true;
+      }
+      const hasAnsweredYes = hasLivedApartSinceSeparationAnswer === 'yes';
+      const hasAnsweredNo = hasLivedApartSinceSeparationAnswer === 'no';
+      const hasGivenDates = livedApartSinceSeparation.length > 0;
+      return hasAnsweredNo || (hasAnsweredYes && hasGivenDates);
+    };
+
+    const fields = {
+      hasLivedApartSinceSeparationAnswer: text.joi(this.content.errors.required, Joi.string()
+        .valid(['yes', 'no'])
+        .required()),
+      livedApartSinceSeparation: text
+    };
+
+    const validation = object(fields)
+      .check(
+        errorFor('livedApartSinceSeparation', this.content.errors.required),
+        validateKillMe);
+
+    return form({ validation });
+  }
 
   answers() {
     const answers = [];
 
     answers.push(answer(this, {
+      question: this.content.fields.hasLivedApartSinceSeparationAnswer.title,
+      answer: this.content.fields
+        .hasLivedApartSinceSeparationAnswer[
+          this.fields.hasLivedApartSinceSeparationAnswer.value]
+    }));
+    answers.push(answer(this, {
       question: this.content.fields.livedApartSinceSeparation.title,
       answer: this.content.fields
-        .livedApartSinceSeparation[this.fields.livedApartSinceSeparation.value]
+        .livedApartSinceSeparation[
+          this.fields.livedApartSinceSeparation.value
+        ]
     }));
-
-    if (this.fields.changes.hasBeenChanges.value === 'yes') {
-      answers.push(answer(this, {
-        question: this.content.fields.livedApartSinceSeparation.title,
-        answer: this.content.fields
-          .livedApartSinceSeparation[this.fields.livedApartSinceSeparation.value]
-      }));
-    } else {
-      answers.push(answer(this, {
-        question: this.content.fields.livedApartSinceSeparation.title,
-        answer: this.content.fields
-          .livedApartSinceSeparation[this.fields.livedApartSinceSeparation.value]
-      }));
-    }
 
     return answers;
   }
