f2857bb5fbf378ab173d946038b98284b0d10e52
John O'Neill <j.oneill@kainos.com>
2018-09-17 17:04:35 +0100

    Updated unit test

--- test/unit/steps/applyForDecreeNisi.test.js
+++ test/unit/steps/applyForDecreeNisi.test.js
@@ -3,6 +3,7 @@ const modulePath = 'steps/apply-for-decree-nisi/ApplyForDecreeNisi.step';
 const ApplyForDecreeNisi = require(modulePath);
 const ApplyForDecreeNisiContent = require('steps/apply-for-decree-nisi/ApplyForDecreeNisi.content');
 const MiniPetition = require('steps/mini-petition/MiniPetition.step');
+const SaveAndClose = require('steps/application-saved-exit/ApplicationSavedExit.step');
 const idam = require('services/idam');
 const { middleware, question, sinon, content } = require('@hmcts/one-per-page-test-suite');
 
@@ -27,9 +28,9 @@ describe(modulePath, () => {
     return question.testErrors(ApplyForDecreeNisi);
   });
 
-  it('redirects to MiniPetition if answer is no', () => {
+  it('redirects to Save & Close page if answer is no', () => {
     const fields = { applyForDecreeNisi: 'no' };
-    return question.redirectWithField(ApplyForDecreeNisi, fields, MiniPetition);
+    return question.redirectWithField(ApplyForDecreeNisi, fields, SaveAndClose);
   });
 
   it('redirects to MiniPetition if answer is yes', () => {
