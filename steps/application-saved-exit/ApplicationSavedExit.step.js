24c39184b7c890e80821a70d4c9b75fe25b21beb
John O'Neill <j.oneill@kainos.com>
2018-09-20 15:06:06 +0100

    Reimplement different session handling

--- steps/application-saved-exit/ApplicationSavedExit.step.js
+++ steps/application-saved-exit/ApplicationSavedExit.step.js
@@ -1,19 +1,22 @@
 const { ExitPoint } = require('@hmcts/one-per-page');
 const config = require('config');
-
-const storePetitionerEmail = (req, res, next) => {
-  res.locals.session.petitionerEmail = req.session.petitionerEmail;
-  next();
-};
+const preserveSession = require('middleware/preserveSession');
 
 class ApplicationSavedExit extends ExitPoint {
-  static get path() {
-    return config.paths.applicationSavedExit;
-  }
+    static get path() {
+        return config.paths.applicationSavedExit;
+    }
 
-  get middleware() {
-    return [storePetitionerEmail, ...super.middleware];
-  }
+    get session() {
+        return this.req.sess;
+    }
+
+    get middleware() {
+        return [
+            preserveSession,
+            ...super.middleware
+        ];
+    }
 }
 
 module.exports = ApplicationSavedExit;
