9ce4e2c5ecd3ce5d746ad28893178675f17464f1
John O'Neill <j.oneill@kainos.com>
2018-09-20 10:12:56 +0100

    DIV-2981: Updated seperation q logic

--- test/unit/steps/livedApartSinceSeparation.test.js
+++ test/unit/steps/livedApartSinceSeparation.test.js
@@ -1,55 +1,49 @@
 const modulePath = 'steps/lived-apart-since-separation/LivedApartSinceSeparation.step';
 
 const LivedApartSinceSeparation = require(modulePath);
-const LivedApartSinceSeparationContent = require(
-  'steps/lived-apart-since-separation/LivedApartSinceSeparation.content'
-);
 const ClaimCosts = require('steps/claim-costs/ClaimCosts.step');
 const idam = require('services/idam');
 const { middleware, question, sinon, content } = require('@hmcts/one-per-page-test-suite');
 
 describe(modulePath, () => {
-  beforeEach(() => {
-    sinon.stub(idam, 'protect').returns(middleware.nextMock);
-  });
+    beforeEach(() => {
+        sinon.stub(idam, 'protect').returns(middleware.nextMock);
+    });
 
-  afterEach(() => {
-    idam.protect.restore();
-  });
+    afterEach(() => {
+        idam.protect.restore();
+    });
 
-  it('has idam.protect middleware', () => {
-    return middleware.hasMiddleware(LivedApartSinceSeparation, [ idam.protect() ]);
-  });
+    it('has idam.protect middleware', () => {
+        return middleware.hasMiddleware(LivedApartSinceSeparation, [ idam.protect() ]);
+    });
 
-  it('renders the content', () => {
-    return content(LivedApartSinceSeparation);
-  });
+    it('renders the content', () => {
+        return content(LivedApartSinceSeparation);
+    });
 
-  it('shows error if does not answer question', () => {
-    return question.testErrors(LivedApartSinceSeparation);
-  });
+    it('shows error if does not answer question', () => {
+        const onlyErrors = ['required'];
+        return question.testErrors(LivedApartSinceSeparation, {}, {}, { onlyErrors });
+    });
 
-  it('redirects to ClaimCosts if answer is no', () => {
-    const fields = { livedApartSinceSeparation: 'no' };
-    return question.redirectWithField(LivedApartSinceSeparation, fields, ClaimCosts);
-  });
+    it('shows error if answered no and no data entered', () => {
+        const onlyErrors = ['requireDatesOfLivingTogether'];
+        const fields = { 'changes-livedApartSinceSeparation': 'no',
+            'changes-approximateDatesOfLivingTogetherField': '' };
+        return question.testErrors(LivedApartSinceSeparation, {}, fields, { onlyErrors });
+    });
 
-  it('redirects to ClaimCosts if answer is yes', () => {
-    const fields = { livedApartSinceSeparation: 'yes' };
-    return question.redirectWithField(LivedApartSinceSeparation, fields, ClaimCosts);
-  });
+    it('redirects to ClaimCosts if answer is no and details are provided', () => {
+        const fields = { 'changes-livedApartSinceSeparation': 'no',
+            'changes-approximateDatesOfLivingTogetherField': 'details...' };
+        return question.redirectWithField(LivedApartSinceSeparation, fields, ClaimCosts);
+    });
 
-  it('loads fields from the session', () => {
-    const sessionData = { livedApartSinceSeparation: 'yes' };
-    return question.rendersValues(LivedApartSinceSeparation, sessionData);
-  });
-
-  it('returns correct answers', () => {
-    const expectedContent = [
-      LivedApartSinceSeparationContent.en.fields.livedApartSinceSeparation.title,
-      LivedApartSinceSeparationContent.en.fields.livedApartSinceSeparation.yes
-    ];
-    const session = { livedApartSinceSeparation: 'yes' };
-    return question.answers(LivedApartSinceSeparation, session, expectedContent);
-  });
+    it('redirects to ClaimCosts if answered yes', () => {
+        const fields = {
+            'changes-livedApartSinceSeparation': 'yes'
+        };
+        return question.redirectWithField(LivedApartSinceSeparation, fields, ClaimCosts);
+    });
 });
