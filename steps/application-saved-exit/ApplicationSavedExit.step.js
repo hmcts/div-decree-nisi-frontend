44fd49abfc884c9ec849ff775060ad1e36d43fae
John O'Neill <j.oneill@kainos.com>
2018-09-20 14:08:03 +0100

    revert session changes

--- steps/application-saved-exit/ApplicationSavedExit.step.js
+++ steps/application-saved-exit/ApplicationSavedExit.step.js
@@ -1,21 +1,18 @@
 const { ExitPoint } = require('@hmcts/one-per-page');
 const config = require('config');
-const preserveSession = require('middleware/preserveSession');
+
+const storePetitionerEmail = (req, res, next) => {
+  res.locals.session.petitionerEmail = req.session.petitionerEmail;
+  next();
+};
 
 class ApplicationSavedExit extends ExitPoint {
   static get path() {
     return config.paths.applicationSavedExit;
   }
 
-  get session() {
-    return this.req.sess;
-  }
-
   get middleware() {
-    return [
-      preserveSession,
-      ...super.middleware
-    ];
+    return [storePetitionerEmail, ...super.middleware];
   }
 }
 
