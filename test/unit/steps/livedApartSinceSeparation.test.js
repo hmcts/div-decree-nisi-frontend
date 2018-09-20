80cd0b0f0c62d4a9adecf85a984181a3c81cd35a
John O'Neill <j.oneill@kainos.com>
2018-09-20 10:51:54 +0100

    Fixed eslint error

--- test/unit/steps/livedApartSinceSeparation.test.js
+++ test/unit/steps/livedApartSinceSeparation.test.js
@@ -6,44 +6,44 @@ const idam = require('services/idam');
 const { middleware, question, sinon, content } = require('@hmcts/one-per-page-test-suite');
 
 describe(modulePath, () => {
-    beforeEach(() => {
-        sinon.stub(idam, 'protect').returns(middleware.nextMock);
-    });
+  beforeEach(() => {
+    sinon.stub(idam, 'protect').returns(middleware.nextMock);
+  });
 
-    afterEach(() => {
-        idam.protect.restore();
-    });
+  afterEach(() => {
+    idam.protect.restore();
+  });
 
-    it('has idam.protect middleware', () => {
-        return middleware.hasMiddleware(LivedApartSinceSeparation, [ idam.protect() ]);
-    });
+  it('has idam.protect middleware', () => {
+    return middleware.hasMiddleware(LivedApartSinceSeparation, [ idam.protect() ]);
+  });
 
-    it('renders the content', () => {
-        return content(LivedApartSinceSeparation);
-    });
+  it('renders the content', () => {
+    return content(LivedApartSinceSeparation);
+  });
 
-    it('shows error if does not answer question', () => {
-        const onlyErrors = ['required'];
-        return question.testErrors(LivedApartSinceSeparation, {}, {}, { onlyErrors });
-    });
+  it('shows error if does not answer question', () => {
+    const onlyErrors = ['required'];
+    return question.testErrors(LivedApartSinceSeparation, {}, {}, { onlyErrors });
+  });
 
-    it('shows error if answered no and no data entered', () => {
-        const onlyErrors = ['requireDatesOfLivingTogether'];
-        const fields = { 'changes-livedApartSinceSeparation': 'no',
-            'changes-approximateDatesOfLivingTogetherField': '' };
-        return question.testErrors(LivedApartSinceSeparation, {}, fields, { onlyErrors });
-    });
+  it('shows error if answered no and no data entered', () => {
+    const onlyErrors = ['requireDatesOfLivingTogether'];
+    const fields = { 'changes-livedApartSinceSeparation': 'no',
+      'changes-approximateDatesOfLivingTogetherField': '' };
+    return question.testErrors(LivedApartSinceSeparation, {}, fields, { onlyErrors });
+  });
 
-    it('redirects to ClaimCosts if answer is no and details are provided', () => {
-        const fields = { 'changes-livedApartSinceSeparation': 'no',
-            'changes-approximateDatesOfLivingTogetherField': 'details...' };
-        return question.redirectWithField(LivedApartSinceSeparation, fields, ClaimCosts);
-    });
+  it('redirects to ClaimCosts if answer is no and details are provided', () => {
+    const fields = { 'changes-livedApartSinceSeparation': 'no',
+      'changes-approximateDatesOfLivingTogetherField': 'details...' };
+    return question.redirectWithField(LivedApartSinceSeparation, fields, ClaimCosts);
+  });
 
-    it('redirects to ClaimCosts if answered yes', () => {
-        const fields = {
-            'changes-livedApartSinceSeparation': 'yes'
-        };
-        return question.redirectWithField(LivedApartSinceSeparation, fields, ClaimCosts);
-    });
+  it('redirects to ClaimCosts if answered yes', () => {
+    const fields = {
+      'changes-livedApartSinceSeparation': 'yes'
+    };
+    return question.redirectWithField(LivedApartSinceSeparation, fields, ClaimCosts);
+  });
 });
