4f1831538bc85a6b0c99ada065f3e897cc8d7784
John O'Neill <j.oneill@kainos.com>
2018-09-20 13:16:52 +0100

    Changed how we handle session

--- steps/application-saved-exit/ApplicationSavedExit.step.js
+++ steps/application-saved-exit/ApplicationSavedExit.step.js
@@ -1,18 +1,21 @@
 const { ExitPoint } = require('@hmcts/one-per-page');
 const config = require('config');
-
-const storePetitionerEmail = (req, res, next) => {
-  res.locals.session.petitionerEmail = req.session.petitionerEmail;
-  next();
-};
+const preserveSession = require('middleware/preserveSession');
 
 class ApplicationSavedExit extends ExitPoint {
   static get path() {
     return config.paths.applicationSavedExit;
   }
 
+  get session() {
+    return this.req.sess;
+  }
+
   get middleware() {
-    return [storePetitionerEmail, ...super.middleware];
+    return [
+      preserveSession,
+      ...super.middleware
+    ];
   }
 }
 
