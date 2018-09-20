232151e1ba911f609fdfb842479f8732465ea10e
John O'Neill <j.oneill@kainos.com>
2018-09-20 10:36:59 +0100

    DIV-2977: ApplyForDN answer directs workflow & save&close page

--- steps/apply-for-decree-nisi/ApplyForDecreeNisi.step.js
+++ steps/apply-for-decree-nisi/ApplyForDecreeNisi.step.js
@@ -1,6 +1,5 @@
-const { Question } = require('@hmcts/one-per-page/steps');
+const { Question, goTo, branch } = require('@hmcts/one-per-page');
 const { form, text } = require('@hmcts/one-per-page/forms');
-const { goTo } = require('@hmcts/one-per-page/flow');
 const { answer } = require('@hmcts/one-per-page/checkYourAnswers');
 const config = require('config');
 const idam = require('services/idam');
@@ -37,8 +36,16 @@ class ApplyForDecreeNisi extends Question {
   }
 
   next() {
-    return goTo(this.journey.steps.MiniPetition);
+    const declinesToApplyForDN = () => {
+      return this.fields.applyForDecreeNisi.value === 'no';
+    };
+
+    return branch(
+      goTo(this.journey.steps.ApplicationSavedExit).if(declinesToApplyForDN),
+      goTo(this.journey.steps.MiniPetition)
+    );
   }
+
 
   get middleware() {
     return [...super.middleware, idam.protect(), getUserData];
