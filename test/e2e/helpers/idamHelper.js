0b82a5ae71c9dc2c2ae785b83c863d42d41322ad
tony.chow <tony.chow@hmcts.net>
2018-09-21 11:14:56 +0100

    Add logs and check for connectivity to idam web

--- test/e2e/helpers/idamHelper.js
+++ test/e2e/helpers/idamHelper.js
@@ -3,11 +3,17 @@ const randomstring = require('randomstring');
 const idamExpressTestHarness = require('@hmcts/div-idam-test-harness');
 const idamConfigHelper = require('./idamConfigHelper');
 
+const request = require('request-promise-native');
+
 const Helper = codecept_helper; // eslint-disable-line
 const args = idamConfigHelper.getArgs();
 
 class IdamHelper extends Helper {
   _before() {
+    request.get('https://idam.preprod.ccidam.reform.hmcts.net').then(response => {
+      console.log(response); // eslint-disable-line no-console
+    });
+
     logger.info('BEFORE BLOCK IS RUN');
     console.log('Before block is run'); // eslint-disable-line no-console
 
@@ -41,12 +47,16 @@ class IdamHelper extends Helper {
   }
 
   _after() {
+    console.log('About to delete new user'); // eslint-disable-line no-console
     return idamExpressTestHarness.removeUser(args)
       .then(() => {
         logger.info(`Removed IDAM test user: ${args.testEmail}`);
+        console.log('Deleted'); // eslint-disable-line no-console
       })
       .catch(error => {
         logger.error(`Unable to remove IDAM test user: ${error}`);
+        console.log('Failed delete'); // eslint-disable-line no-console
+        console.log(error); // eslint-disable-line no-console
       });
   }
 }
