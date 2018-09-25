a42c5134fa092123f09fe3aba2165dff9da191ba
Tony Chow <36265570+tchow8@users.noreply.github.com>
2018-09-25 14:23:07 +0100

    [PREVIEW] Pass in reform environment on tests (#62)

--- test/e2e/pages/idam.step.js
+++ test/e2e/pages/idam.step.js
@@ -19,7 +19,7 @@ async function loginToIdam(success = true) {
     I.fillField('username', idamConfigHelper.getTestEmail());
     I.fillField('password', idamConfigHelper.getTestPassword());
     I.navByClick('Sign in');
-    I.wait(2);
+    I.wait(3);
   }
 }
 
