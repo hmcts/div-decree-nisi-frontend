53ebf86e5419d7e5456512976cb8b9a62bec3b6d
Mounika Ammineni <mounikaa@BOSE.local>
2018-09-25 16:09:52 +0100

    Reverting date format chagnes

--- steps/behaviour-continued-since-application/BehaviourContinuedSinceApplication.step.js
+++ steps/behaviour-continued-since-application/BehaviourContinuedSinceApplication.step.js
@@ -43,7 +43,7 @@ class BehaviourContinuedSinceApplication extends Question {
     const fields = {
       behaviourContinuedSinceApplication,
       lastIncidentDate: convert(
-        d => moment(`${d.day}/${d.month}/${d.year}`, 'DD/MM/YYYY'), // eslint-disable-line
+        d => moment(`${d.year}-${d.month}-${d.day}`, 'YYYY-MM-DD'), // eslint-disable-line
         date
       )
     };
