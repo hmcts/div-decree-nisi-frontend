603ad3abca010045d2b3e140fe880b78758cc53a
Tony Chow <36265570+tchow8@users.noreply.github.com>
2018-09-27 13:00:05 +0100

    Update healthcheck.js

--- middleware/healthcheck.js
+++ middleware/healthcheck.js
@@ -1,15 +1,6 @@
 const healthcheck = require('@hmcts/nodejs-healthcheck');
 const config = require('config');
 const os = require('os');
-const logger = require('@hmcts/nodejs-logging').Logger.getLogger(__filename);
-const redis = require('services/redis');
-const outputs = require('@hmcts/nodejs-healthcheck/healthcheck/outputs');
-const { OK } = require('http-status-codes');
-
-const options = {
-  timeout: config.health.timeout,
-  deadline: config.health.deadline
-};
 
 const checks = () => {
   return {};
