76b6d3fb1cd952478af03ff43eec5634a3b0ca11
Dominic Taylor <dom.taylor7@gmail.com>
2018-10-02 09:46:06 +0100

    test cos service

--- test/unit/services/caseOrchestrationService.test.js
+++ test/unit/services/caseOrchestrationService.test.js
@@ -1,12 +1,36 @@
 const moduleName = 'services/caseOrchestrationService';
 
 const caseOrchestrationService = require(moduleName);
-const { expect } = require('@hmcts/one-per-page-test-suite');
+const request = require('request-promise-native');
+const config = require('config');
+const { expect, sinon } = require('@hmcts/one-per-page-test-suite');
+
+const exampleCosResponse = { foo: 'bar' };
 
 describe(moduleName, () => {
-  it('defines get application', () => {
-    expect(caseOrchestrationService.hasOwnProperty('getApplication')).to.eql(true);
+  beforeEach(() => {
+    sinon.stub(request, 'get');
   });
+
+  afterEach(() => {
+    request.get.restore();
+  });
+
+  it('gets application from cos', done => {
+    request.get.resolves(exampleCosResponse);
+    const req = { cookies: { '__auth-token': 'token' } };
+
+    const uri = `${config.services.orchestrationService.getCaseUrl}?checkCcd=true`;
+    const headers = { Authorization: 'Bearer token' };
+
+    caseOrchestrationService.getApplication(req)
+      .then(response => {
+        sinon.assert.calledWith(request.get, { uri, headers, json: true });
+        expect(response).to.eql(exampleCosResponse);
+      })
+      .then(done, done);
+  });
+
   it('defines submit application', () => {
     expect(caseOrchestrationService.hasOwnProperty('submitApplication')).to.eql(true);
   });
