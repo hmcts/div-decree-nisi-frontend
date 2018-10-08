61e4558567db7e5a6cfe833c2e1a1bcd36f2208d
Dominic Taylor <dom.taylor7@gmail.com>
2018-10-08 10:37:41 +0100

    update application flow

--- steps/index.js
+++ steps/index.js
@@ -7,7 +7,10 @@ const getSteps = () => {
   glob.sync('steps/**/*.step.js').forEach(file => {
     const step = require(file); // eslint-disable-line global-require
 
-    steps.push(step);
+    const stepEnabled = step.hasOwnProperty('stepEnabled') ? step.stepEnabled : true;
+    if (stepEnabled) {
+      steps.push(step);
+    }
   });
 
   if (['development'].includes(config.environment)) {
--- steps/start/Start.step.js
+++ steps/start/Start.step.js
@@ -1,14 +1,24 @@
-const { Page } = require('@hmcts/one-per-page');
+const { Redirect, branch } = require('@hmcts/one-per-page');
+const { redirectTo } = require('@hmcts/one-per-page/flow');
 const config = require('config');
 const idam = require('services/idam');
 
-class Start extends Page {
-  static get ignorePa11yWarnings() {
-    return ['WCAG2AA.Principle1.Guideline1_3.1_3_1.H48'];
-  }
+class Start extends Redirect {
   static get path() {
     return config.paths.index;
   }
+
+  next() {
+    const hasSession = () => {
+      return this.req.session && this.req.session.case;
+    };
+
+    return branch(
+      redirectTo(this.journey.steps.Undefended).if(hasSession),
+      redirectTo(this.journey.steps.Entry)
+    );
+  }
+
   get middleware() {
     return [...super.middleware, idam.setRedirectUri];
   }
