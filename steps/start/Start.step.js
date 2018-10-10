e99c6adf7f7f4352eeac177b0c0862ae127cf231
Dominic Taylor <dom.taylor7@gmail.com>
2018-10-10 10:08:55 +0100

    remove commented out code

--- steps/start/Start.step.js
+++ steps/start/Start.step.js
@@ -37,32 +37,3 @@ class Start extends Page {
 }
 
 module.exports = Start;
-
-
-// const { Redirect, branch } = require('@hmcts/one-per-page');
-// const { redirectTo } = require('@hmcts/one-per-page/flow');
-// const config = require('config');
-// const idam = require('services/idam');
-//
-// class Start extends Redirect {
-//   static get path() {
-//     return config.paths.index;
-//   }
-//
-//   next() {
-//     const hasSession = () => {
-//       return this.req.session && this.req.session.case;
-//     };
-//
-//     return branch(
-//       redirectTo(this.journey.steps.Undefended).if(hasSession),
-//       redirectTo(this.journey.steps.Entry)
-//     );
-//   }
-//
-//   get middleware() {
-//     return [...super.middleware, idam.setRedirectUri];
-//   }
-// }
-//
-// module.exports = Start;
