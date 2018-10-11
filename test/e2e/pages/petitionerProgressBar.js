4be0b4b4c550e64dca6f77e16489a3e30923ae46
Mounika <mounika.ammineni@hmcts.net>
2018-10-11 14:58:32 +0100

    Functional page changes

--- test/e2e/pages/petitionerProgressBar.js
+++
@@ -1,9 +0,0 @@
-const progressBarStep = require('steps/petition-progress-bar/PetitionProgressBar.step');
-
-function seeProgressBar() {
-  const I = this;
-
-  I.seeCurrentUrlEquals(progressBarStep.path);
-}
-
-module.exports = { seeProgressBar };
---
+++ test/functional/pages/petitionerProgressBar.js
@@ -0,0 +1,13 @@
+const ProgressBarStep = require('steps/petition-progress-bar/PetitionProgressBar.step');
+
+const ProgressBarStepContent = require('steps/petition-progress-bar/PetitionProgressBar.content');
+
+function testProgressBar() {
+  const I = this;
+
+
+  I.amOnLoadedPage(ProgressBarStep.path);
+  I.seeCurrentUrlEquals(ProgressBarStepContent.en.title);
+}
+
+module.exports = { testProgressBar };
