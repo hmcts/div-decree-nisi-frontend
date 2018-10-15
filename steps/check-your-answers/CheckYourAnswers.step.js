30930004cdd6ddc796dd3983dd4a2642e9526e8b
Dominic Taylor <dom.taylor7@gmail.com>
2018-10-15 17:08:20 +0100

    add submission to cos (#86)

--- steps/check-your-answers/CheckYourAnswers.step.js
+++ steps/check-your-answers/CheckYourAnswers.step.js
@@ -3,6 +3,8 @@ const { goTo, action, redirectTo } = require('@hmcts/one-per-page/flow');
 const config = require('config');
 const idam = require('services/idam');
 const caseOrchestrationService = require('services/caseOrchestrationService');
+const { form, text } = require('@hmcts/one-per-page/forms');
+const Joi = require('joi');
 
 class CheckYourAnswers extends CYA {
   static get path() {
@@ -16,7 +18,7 @@ class CheckYourAnswers extends CYA {
   next() {
     return action(caseOrchestrationService.submitApplication)
       .then(goTo(this.journey.steps.Done))
-      .onFailure(redirectTo(this.journey.steps.Start));
+      .onFailure(redirectTo(this.journey.steps.CheckYourAnswers));
   }
 
   get errorMessage() {
@@ -25,6 +27,16 @@ class CheckYourAnswers extends CYA {
 
   get middleware() {
     return [...super.middleware, idam.protect()];
+  }
+
+  get form() {
+    const answers = ['yes'];
+    return form({
+      statementOfTruth: text.joi(
+        this.errorMessage,
+        Joi.required().valid(answers)
+      )
+    });
   }
 }
 
