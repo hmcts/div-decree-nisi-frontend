407138ae847f85750976198c552afe246c3abcf2
Dominic Taylor <dom.taylor7@gmail.com>
2018-10-22 16:19:13 +0100

    test idam as part of smoke

--- test/functional/smoke/index.js
+++ test/functional/smoke/index.js
@@ -1,6 +1,9 @@
 Feature('Smoke test', { retries: 2 });
 
-Scenario('Can see index page', I => {
+Scenario('Can see index page', async I => {
   I.amOnLoadedPage('/');
+
   I.testHomePage();
+
+  await I.testIdamPage();
 });
