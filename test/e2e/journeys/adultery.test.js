e21c58fa42a513d3a1de8809b8a4c14b303c7854
Mounika <mounika.ammineni@hmcts.net>
2018-11-05 16:36:26 +0000

    Adding e2e tests

---
+++ test/e2e/journeys/adultery.test.js
@@ -0,0 +1,76 @@
+const { journey, sinon } = require('@hmcts/one-per-page-test-suite');
+const request = require('request-promise-native');
+const { merge } = require('lodash');
+const mockCaseResponse = require('mocks/services/case-orchestration/retrieve-aos-case/mock-case');
+const config = require('config');
+
+const Start = require('steps/start/Start.step');
+const IdamLogin = require('mocks/steps/idamLogin/IdamLogin.step');
+const petitionProgressBar = require('steps/petition-progress-bar/PetitionProgressBar.step');
+const ApplyForDecreeNisi = require('steps/apply-for-decree-nisi/ApplyForDecreeNisi.step');
+const MiniPetition = require('steps/mini-petition/MiniPetition.step');
+const Intolerable = require('steps/intolerable/Intolerable.step');
+const AdulteryFirstFoundOut = require('steps/adultery-first-found-out/AdulteryFirstFoundOut.step');
+const LivedApartSinceAdultery = require(
+  'steps/lived-apart-since-adultery/LivedApartSinceAdultery.step'
+);
+
+const ClaimCosts = require('steps/claim-costs/ClaimCosts.step');
+const ShareCourtDocuments = require('steps/share-court-documents/ShareCourtDocuments.step');
+const CheckYourAnswers = require('steps/check-your-answers/CheckYourAnswers.step');
+const Done = require('steps/done/Done.step');
+
+const session = {
+  reasonForDivorce: 'adultery',
+  respDefendsDivorce: null,
+  reasonForDivorceAdulteryDetails: 'details'
+};
+
+describe('Adultery DN flow', () => {
+  before(() => {
+    const getStub = sinon.stub(request, 'get');
+    const postStub = sinon.stub(request, 'post');
+
+    getStub
+      .withArgs(sinon.match({
+        uri: `${config.services.orchestrationService.getCaseUrl}?checkCcd=true`
+      }))
+      .resolves(merge({}, mockCaseResponse, { data: session }));
+
+    postStub
+      .withArgs(sinon.match({
+        uri: `${config.services.orchestrationService.submitCaseUrl}/${mockCaseResponse.caseId}`
+      }))
+      .resolves();
+  });
+
+  after(() => {
+    request.get.restore();
+    request.post.restore();
+  });
+
+  journey.test([
+    { step: Start },
+    { step: IdamLogin, body: { success: 'yes' } },
+    { step: petitionProgressBar },
+    { step: ApplyForDecreeNisi, body: { applyForDecreeNisi: 'yes' } },
+    {
+      step: MiniPetition,
+      body: {
+        'changes-hasBeenChanges': 'no',
+        'changes-statementOfTruthNoChanges': 'yes'
+      }
+    },
+    { step: Intolerable, body: { intolerable: 'yes' } },
+    { step: AdulteryFirstFoundOut, body: {
+      'changes-adulteryFirstFoundDate-day': '09',
+      'changes-adulteryFirstFoundDate-month': '08',
+      'changes-adulteryFirstFoundDate-year': '2011'
+    } },
+    { step: LivedApartSinceAdultery, body: { 'livedApart-livedApartSinceAdultery': 'yes' } },
+    { step: ClaimCosts, body: { claimCosts: 'originalAmount' } },
+    { step: ShareCourtDocuments, body: { upload: 'no' } },
+    { step: CheckYourAnswers, body: { statementOfTruth: 'yes' } },
+    { step: Done }
+  ]);
+});
