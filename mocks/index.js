7d5e43b6cfa97513c64d6f80272824a810bd69b5
Mathan Thava <mathan.thavachelvam@hmcts.net>
2018-11-13 20:34:48 +0000

    set mock port to 4001

--- mocks/index.js
+++ mocks/index.js
@@ -3,7 +3,7 @@ const mockserver = require('mockserver');
 
 function startCaseOrchestrationMock() {
   // This is a file-based mock server for local development/local E2E tests
-  const port = 3001;
+  const port = 4001;
   const mocksPath = 'mocks/services/case-orchestration';
   http.createServer(mockserver(mocksPath, true))
     .listen(port);
