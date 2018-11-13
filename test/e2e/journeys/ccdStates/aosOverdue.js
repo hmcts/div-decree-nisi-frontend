2c2337057996a354878a5aeee6d3cc64afdfea54
mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
2018-11-13 12:13:40 +0000

    Div 3463 e2 (#128)
    * Adding e2e tests
    
    * E2E journesy for all ccd states
    
    * DNAwaiting state e2e added
    
    * add new helper for COS formatting the request
    
    * cherry pick
    
    * Cherrypick
    
    * modify moment json helper again
    
    * Added dates as moment for now
    
    * Added custom matcher to verify request param content
    
    * Date formatted

--- test/e2e/journeys/ccdStates/aosOverdue.js
+++ test/e2e/journeys/ccdStates/aosOverdue.js
@@ -18,13 +18,12 @@ const CheckYourAnswers = require('steps/check-your-answers/CheckYourAnswers.step
 const Done = require('steps/done/Done.step');
 
 const session = {
-  reasonForDivorce: 'separation-2-years',
   respDefendsDivorce: null
 };
 
 let caseOrchestrationServiceSubmitStub = {};
 
-describe('Sepereration 2 years', () => {
+describe('Case State : AOSOverdue', () => {
   before(() => {
     const getStub = sinon.stub(request, 'get');
     const postStub = sinon.stub(request, 'post');
@@ -33,7 +32,7 @@ describe('Sepereration 2 years', () => {
       .withArgs(sinon.match({
         uri: `${config.services.orchestrationService.getCaseUrl}?checkCcd=true`
       }))
-      .resolves(merge({}, mockCaseResponse, { data: session }));
+      .resolves(merge({}, mockCaseResponse, { state: 'DefendedDivorce', data: session }));
 
     caseOrchestrationServiceSubmitStub = postStub
       .withArgs(sinon.match({
@@ -46,6 +45,7 @@ describe('Sepereration 2 years', () => {
     request.get.restore();
     request.post.restore();
   });
+
 
   journey.test([
     { step: Start },
@@ -68,10 +68,10 @@ describe('Sepereration 2 years', () => {
 
   it('submits correct body to case orchestration service', () => {
     const body = {
-      statementOfTruthChanges: 'yes',
       claimCosts: 'originalAmount',
+      livedApartSinceSeparation: 'yes',
       statementOfTruth: 'yes',
-      livedApartSinceSeparation: 'yes'
+      statementOfTruthChanges: 'yes'
     };
     sinon.assert.calledWith(caseOrchestrationServiceSubmitStub, sinon.match.has('body', body));
   });
