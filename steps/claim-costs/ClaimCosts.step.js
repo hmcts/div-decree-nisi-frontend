81b75f4b68159a2ea0ce8a269a7be4f44f545fe9
Mounika <mounika.ammineni@hmcts.net>
2018-11-14 20:58:03 +0000

    base changes

--- steps/claim-costs/ClaimCosts.step.js
+++ steps/claim-costs/ClaimCosts.step.js
@@ -1,5 +1,5 @@
 const { Question } = require('@hmcts/one-per-page/steps');
-const { form, text } = require('@hmcts/one-per-page/forms');
+const { form, text, errorFor, object } = require('@hmcts/one-per-page/forms');
 const { redirectTo } = require('@hmcts/one-per-page/flow');
 const { answer } = require('@hmcts/one-per-page/checkYourAnswers');
 const config = require('config');
@@ -8,8 +8,8 @@ const Joi = require('joi');
 
 const constants = {
   originalAmount: 'originalAmount',
-  specificAmount: 'specificAmount',
-  endClaim: 'endClaim',
+  specificAmount: 'differentAmount',
+  dontClaim: 'endClaim',
   respAgreeToCosts: 'respAgreeToCosts',
   yes: 'Yes',
   no: 'No'
@@ -36,24 +36,55 @@ class ClaimCosts extends Question {
     const answers = [
       this.consts.originalAmount,
       this.consts.specificAmount,
-      this.consts.endClaim
+      this.consts.dontClaim
     ];
+
     const validAnswers = Joi.string()
       .valid(answers)
       .required();
 
-    const claimCosts = text
-      .joi(this.content.errors.required, validAnswers);
+    const fields = {
+      claimCosts: text
+        .joi(this.content.errors.required, validAnswers),
+      costsDifferentDetails: text
+    };
 
-    return form({ claimCosts });
+    const validateCostsDifferentDetails = ({ claimCosts = '', costsDifferentDetails = '' }) => {
+      // only validate if user has answered hasBeenChanges
+      const isSpecAmntSelected = claimCosts === this.consts.specificAmount;
+      if (!isSpecAmntSelected) {
+        return true;
+      }
+      return costsDifferentDetails.length > 0;
+    };
+
+    const dnCosts = object(fields)
+      .check(
+        errorFor('costsDifferentDetails', this.content.errors.requiredCostsDifferentDetails),
+        validateCostsDifferentDetails
+      );
+
+    return form({ dnCosts });
   }
 
   answers() {
-    return answer(this, {
-      question: this.content.fields.claimCosts.title,
-      answer: this.content.fields.claimCosts[this.fields.claimCosts.value]
-    });
+    const answers = [];
+
+    answers.push(answer(this, {
+      question: this.content.fields.dnCosts.title,
+      answer: this.content.fields.dnCosts[this.fields.dnCosts.claimCosts.value]
+    }));
+
+    if (this.fields.dnCosts.claimCosts.value === this.consts.specificAmount) {
+      answers.push(answer(this, {
+        question: this.content.fields.dnCosts.costsDifferentDetails.title,
+        answer: this.fields.dnCosts.costsDifferentDetails.value
+      }));
+    }
+
+    return answers;
   }
+
 
   next() {
     return redirectTo(this.journey.steps.ShareCourtDocuments);
