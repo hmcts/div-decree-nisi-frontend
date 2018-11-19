3de700ce38e8896462514c7127b0bd402e7a5bbd
Hosain <hosainnet@users.noreply.github.com>
2018-11-19 10:00:07 +0000

    DIV-3491 - create functional integration case (#141)
    * WIP: over to Hosain
    
    * add intergration test using codecept
    
    * DIV-3491 - create a new case for every run of the functional tests
    
    * create a new user for every run of the functional tests
    * create an adultery case for that user which is in ready for DN state
    * refactored proxy env variables to be more generic
    * added IDAM feature flag
    * changed functional test command to run functional test (not smoke)
    
    * DIV-3491 - moves basic-divorce-session to test/resources
    
    * Adding dependency to package.json
    
    * fixed integration test
    
    * DIV-3491 - renamed IdamHelper.js to idamHelper.js
    
    * wait for user/case creation
    
    * blank
    
    * update yarn
    
    * increase timeouts
    
    * dont look for content
    
    * div-idam-test-harness version
    
    * dependancies
    
    * logs
    
    * DIV-3491 - updated to div-test-harness 0.1 and increased test log level
    
    Pass case data directly to createDnCaseForUser instead of file path
    
    * DIV-3491 - added required D8MarriagePlaceOfMarriage field
    
    * set idam uri on each request
    
    * DIV-3491 - added missing env params for functional tests
    
    * mock json
    
    * fix pages
    
    * intergration only
    
    * test
    
    * fix feature idam
    
    * fixs
    
    * fixs
    
    * DIV-3491 - fixed incorrect env variable name
    
    It should be FEATURES_IDAM not FEATURE_IDAM
    
    * fixs
    
    * try await
    
    * use smoke for functional
    
    * add waiter
    
    * repalce functional for smoke
    
    * versions

--- test/functional/pages/idam.step.js
+++ test/functional/pages/idam.step.js
@@ -2,6 +2,7 @@ const IdamMockLogin = require('mocks/steps/idamLogin/IdamLogin.step');
 const content = require('mocks/steps/idamLogin/IdamLogin.content');
 const PetitionProgressBarPage = require('steps/petition-progress-bar/PetitionProgressBar.step');
 const StartPageContent = require('steps/start/Start.content');
+const idamConfigHelper = require('test/functional/helpers/idamConfigHelper.js');
 
 async function testIdamPage(success = true) {
   const I = this;
@@ -21,9 +22,9 @@ async function testIdamPage(success = true) {
       }
       I.navByClick('Continue');
     } else {
-      I.seeInCurrentUrl('/login?');
-      I.fillField('username', 'divdecreenisitesting@mailinator.com');
-      I.fillField('password', 'Test1234');
+      await I.seeInCurrentUrl('/login?');
+      I.fillField('username', idamConfigHelper.getTestEmail());
+      I.fillField('password', idamConfigHelper.getTestPassword());
       I.navByClick('Sign in');
       I.wait(3);
     }
