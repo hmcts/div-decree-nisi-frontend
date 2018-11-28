e76776d6765c7add7ef2e4de314224813e07d2ac
Dominic Taylor <dom.taylor7@gmail.com>
2018-11-28 12:54:22 +0000

    secure cookies (#168)
    * secure cookies
    
    * secure cookies
    
    * trust proxy

--- app.js
+++ app.js
@@ -80,13 +80,18 @@ app.use('/images', (req, res) => {
   res.redirect(`/assets/images${req.path}`, '301');
 });
 
+app.set('trust proxy', 1);
+
 onePerPage.journey(app, {
   baseUrl: config.node.baseUrl,
   steps: getSteps(),
   errorPages: { serverError: { template: 'errors/error' } },
   session: {
     redis: { url: config.services.redis.url },
-    cookie: { secure: config.services.redis.useSSL === 'true' },
+    cookie: {
+      secure: config.session.secure,
+      expires: config.session.expires
+    },
     secret: config.session.secret,
     sessionEncryption: req => {
       let key = config.services.redis.encryptionAtRestKey;
