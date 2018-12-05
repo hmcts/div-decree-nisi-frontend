b67144c9213370060db3b3edb098565e1f60a5e4
J. Victor Ferreira <jvictor.ferreira@jworks.eu>
2018-12-05 12:00:20 +0000

    Changed the way court information is displayed so that CTSC

--- test/unit/steps/dnNoResponse.test.js
+++ test/unit/steps/dnNoResponse.test.js
@@ -1,6 +1,9 @@
 const DnNoResponse = require('steps/dn-no-response/DnNoResponse.step');
-
-const { content } = require('@hmcts/one-per-page-test-suite');
+const DnNoResponseContent = require('steps/dn-no-response/DnNoResponse.content');
+const { custom, expect, content } = require('@hmcts/one-per-page-test-suite');
+const httpStatus = require('http-status-codes');
+const { getExpectedCourtsList, testDivorceUnitDetailsRender,
+  testCTSCDetailsRender } = require('test/unit/helpers/courtInformation');
 
 describe('DnNoResponse step', () => {
   it('renders the content', () => {
@@ -12,5 +15,55 @@ describe('DnNoResponse step', () => {
       'doneWithAllWaysOfDelivering.applyD13bLink'
     ];
     return content(DnNoResponse, session, { specificContent, ignoreContent });
+  });
+
+  describe('court address details', () => {
+    it('should display divorce center details when divorce unit handles case', () => {
+      const session = {
+        case: {
+          data: {
+            courts: 'westMidlands',
+            court: getExpectedCourtsList()
+          }
+        }
+      };
+
+      return custom(DnNoResponse)
+        .withSession(session)
+        .get()
+        .expect(httpStatus.OK)
+        .html($ => {
+          const rightHandSideMenu = $('.column-one-third').html();
+
+          testDivorceUnitDetailsRender(rightHandSideMenu);
+          expect(rightHandSideMenu).to.include(DnNoResponseContent.en.openTimes)
+            .and.to.include(DnNoResponseContent.en.divorceEmail)
+            .and.to.include(DnNoResponseContent.en.phoneNumber);
+        });
+    });
+
+    it('should display service center details when service centre handles case', () => {
+      const session = {
+        case: {
+          data: {
+            courts: 'serviceCentre',
+            court: getExpectedCourtsList()
+          }
+        }
+      };
+
+      return custom(DnNoResponse)
+        .withSession(session)
+        .get()
+        .expect(httpStatus.OK)
+        .html($ => {
+          const rightHandSideMenu = $('.column-one-third').html();
+
+          testCTSCDetailsRender(rightHandSideMenu);
+          expect(rightHandSideMenu).to.include(DnNoResponseContent.en.openTimes)
+            .and.to.include(DnNoResponseContent.en.divorceEmail)
+            .and.to.include(DnNoResponseContent.en.phoneNumber);
+        });
+    });
   });
 });
