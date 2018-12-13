ba6ba2e190a04914d9313034653c3a0ab8810a79
Dominic Taylor <dom.taylor7@gmail.com>
2018-12-13 13:07:25 +0000

    use proxy for idam idam-authentication healthcheck & refactor healthcheck tests

--- middleware/healthcheck.js
+++ middleware/healthcheck.js
@@ -5,6 +5,7 @@ const logger = require('@hmcts/nodejs-logging').Logger.getLogger(__filename);
 const redis = require('services/redis');
 const outputs = require('@hmcts/nodejs-healthcheck/healthcheck/outputs');
 const { OK } = require('http-status-codes');
+const request = require('request-promise-native');
 
 const options = {
   timeout: config.health.timeout,
@@ -14,22 +15,33 @@ const options = {
 const checks = () => {
   return {
     redis: healthcheck.raw(() => {
-      return redis.ping().then(_ => {
-        return healthcheck.status(_ === 'PONG');
-      })
+      return redis.ping()
+        .then(_ => {
+          return healthcheck.status(_ === 'PONG');
+        })
         .catch(error => {
           logger.error(`Health check failed on redis: ${error}`);
-          return false;
+          return healthcheck.status(false);
         });
     }),
-    'idam-authentication': healthcheck.web(config.services.idam.authenticationHealth, {
-      callback: (error, res) => { // eslint-disable-line id-blacklist
-        if (error) {
+    'idam-authentication': healthcheck.raw(() => {
+      const proxyRequest = request
+        .defaults({ proxy: config.services.idam.proxy });
+      const requestOptions = Object.assign(
+        { uri: config.services.idam.authenticationHealth },
+        options
+      );
+
+      return proxyRequest(requestOptions)
+        .then(body => {
+          const response = JSON.parse(body);
+          return healthcheck.status(response);
+        })
+        .catch(error => {
           logger.error(`Health check failed on idam-authentication: ${error}`);
-        }
-        return !error && res.status === OK ? outputs.up() : outputs.down(error);
-      }
-    }, options),
+          return healthcheck.status(false);
+        });
+    }),
     'idam-app': healthcheck.web(config.services.idam.apiHealth, {
       callback: (error, res) => { // eslint-disable-line id-blacklist
         if (error) {
