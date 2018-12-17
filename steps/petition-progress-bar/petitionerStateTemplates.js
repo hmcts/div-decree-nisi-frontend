aa9ed1b889936e562740492829b05b7880a6f3a6
tony.chow <tony.chow@hmcts.net>
2018-12-17 13:28:57 +0000

    Redirect AwaitingConsideration to DnSubmitted Awaiting Response Petitioner Progress Bar Page

--- steps/petition-progress-bar/petitionerStateTemplates.js
+++ steps/petition-progress-bar/petitionerStateTemplates.js
@@ -9,7 +9,7 @@ const caseStateMap = [
   },
   {
     template: './sections/awaitingSubmittedDN/PetitionProgressBar.awaitingSubmittedDN.template.html',
-    state: ['awaitinglegaladvisorreferral']
+    state: ['awaitinglegaladvisorreferral', 'awaitingconsideration']
   },
   {
     template: './sections/defendedWithAnswer/PetitionProgressBar.defendedWithAnswer.template.html',
