27671c76bad1b2ce3ed29932127dbdeabd624811
Mounika <mounika.ammineni@hmcts.net>
2019-01-02 07:01:16 +0000

    Added base for the adultery resp not admit question

---
+++ steps/resp-not-admit-adultery/RespNotAdmitAdultery.step.js
@@ -0,0 +1,55 @@
+const { Question, branch } = require('@hmcts/one-per-page');
+const { redirectTo } = require('@hmcts/one-per-page/flow');
+const { form, text } = require('@hmcts/one-per-page/forms');
+const { answer } = require('@hmcts/one-per-page/checkYourAnswers');
+const config = require('config');
+const idam = require('services/idam');
+const Joi = require('joi');
+
+class RespNotAdmitAdultery extends Question {
+  static get path() {
+    return config.paths.respNotAdmitAdultery;
+  }
+
+  get case() {
+    return this.req.session.case.data;
+  }
+
+  get form() {
+    const answers = ['yes', 'no'];
+    const validAnswers = Joi.string()
+      .valid(answers)
+      .required();
+
+    const amendPetition = text
+      .joi(this.content.errors.required, validAnswers);
+
+    return form({ amendPetition });
+  }
+
+  answers() {
+    return answer(this, {
+      question: this.content.fields.amendPetition.title,
+      answer: this.content.fields
+        .amendPetition[this.fields.amendPetition.value]
+    });
+  }
+
+  next() {
+    const amendPetition = () => {
+      return this.fields.amendPetition.value === 'yes';
+    };
+
+    return branch(
+      redirectTo(this.journey.steps.Exit).if(amendPetition),
+      redirectTo(this.journey.steps.ApplyForDecreeNisi)
+    );
+  }
+
+
+  get middleware() {
+    return [...super.middleware, idam.protect()];
+  }
+}
+
+module.exports = RespNotAdmitAdultery;
