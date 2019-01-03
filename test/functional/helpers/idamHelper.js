a743e950a747b46534dab83541dd2bba38fd2013
John O'Neill <j.oneill@kainos.com>
2019-01-03 11:45:19 +0000

    DIV-3830: Updated email to be more verbose

--- test/functional/helpers/idamHelper.js
+++ test/functional/helpers/idamHelper.js
@@ -23,7 +23,7 @@ class IdamHelper extends Helper {
         length: 16,
         charset: 'numeric'
       });
-      const emailName = `hmcts.divorce.reform-${randomString}`;
+      const emailName = `hmcts.divorce.reform+automatedtest-${randomString}`;
       const testEmail = `${emailName}@gmail.com`;
       const testPassword = randomstring.generate(9);
 
