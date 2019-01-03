43a3cb5253503e8e885600f385d265cedba38191
Mounika <mounika.ammineni@hmcts.net>
2019-01-03 08:51:37 +0000

    Functional tests added

--- test/e2e/journeys/DivorceReasons/adultery.test.js
+++ test/e2e/journeys/DivorceReasons/adultery.test.js
@@ -7,6 +7,8 @@ const config = require('config');
 const Start = require('steps/start/Start.step');
 const IdamLogin = require('mocks/steps/idamLogin/IdamLogin.step');
 const petitionProgressBar = require('steps/petition-progress-bar/PetitionProgressBar.step');
+const reviewAosResponse = require('steps/review-aos-response/ReviewAosResponse.step');
+const respNotAdmitAdultery = require('steps/resp-not-admit-adultery/RespNotAdmitAdultery.step');
 const ApplyForDecreeNisi = require('steps/apply-for-decree-nisi/ApplyForDecreeNisi.step');
 const MiniPetition = require('steps/mini-petition/MiniPetition.step');
 const Intolerable = require('steps/intolerable/Intolerable.step');
@@ -171,5 +173,84 @@ describe('Adultery DN flow', () => {
       { step: Intolerable, body: { intolerable: 'no' } },
       { step: ExitIntolerable }
     ]);
+  });
+});
+
+
+describe('Respondent Admitted Adultery : no', () => {
+  const sess = {
+    reasonForDivorce: 'adultery',
+    respDefendsDivorce: 'Yes',
+    respAdmitOrConsentToFact: 'No',
+    reasonForDivorceAdulteryDetails: 'details'
+  };
+
+  before(() => {
+    const getStub = sinon.stub(request, 'get');
+    const postStub = sinon.stub(request, 'post');
+
+    getStub
+      .withArgs(sinon.match({
+        uri: `${config.services.orchestrationService.getCaseUrl}`
+      }))
+      .resolves(merge({}, mockCaseResponse, { data: sess }));
+
+    caseOrchestrationServiceSubmitStub = postStub
+      .withArgs(sinon.match({
+        uri: `${config.services.orchestrationService.submitCaseUrl}/${mockCaseResponse.caseId}`
+      }));
+    caseOrchestrationServiceSubmitStub.resolves();
+  });
+
+  after(() => {
+    request.get.restore();
+    request.post.restore();
+  });
+
+  describe('Intolerable: yes, livedApartSinceAdultery: yes', () => {
+    journey.test([
+      { step: Start },
+      { step: IdamLogin, body: { success: 'yes' } },
+      { step: Entry },
+      { step: petitionProgressBar },
+      { step: reviewAosResponse, body: { reviewAosResponse: 'yes' } },
+      { step: respNotAdmitAdultery, body: { amendPetition: 'no' } },
+      { step: ApplyForDecreeNisi, body: { applyForDecreeNisi: 'yes' } },
+      {
+        step: MiniPetition,
+        body: {
+          'changes-hasBeenChanges': 'no',
+          'changes-statementOfTruthNoChanges': 'yes'
+        }
+      },
+      { step: Intolerable, body: { intolerable: 'yes' } },
+      { step: AdulteryFirstFoundOut, body: {
+        'adulteryFirstFoundDate-day': '09',
+        'adulteryFirstFoundDate-month': '08',
+        'adulteryFirstFoundDate-year': '2011'
+      } },
+      { step: LivedApartSinceAdultery, body: { 'livedApart-livedApartSinceAdultery': 'yes' } },
+      { step: ClaimCosts, body: { 'dnCosts-claimCosts': 'originalAmount' } },
+      { step: ShareCourtDocuments, body: { upload: 'no' } },
+      { step: CheckYourAnswers, body: { statementOfTruth: 'yes' } },
+      { step: Done }
+    ]);
+
+    it('submits correct body to case orchestration service', () => {
+      const body = {
+        applyForDecreeNisi: 'yes',
+        statementOfTruthChanges: 'yes',
+        claimCosts: 'originalAmount',
+        statementOfTruth: 'yes',
+        intolerable: 'yes',
+        adulteryFirstFoundDate: moment('2011-08-09T00:00:00.000'),
+        livedApartSinceAdultery: 'yes'
+      };
+      sinon.assert.calledWith(
+        caseOrchestrationServiceSubmitStub,
+        sinon.match(matchParam('body', body)
+        )
+      );
+    });
   });
 });
