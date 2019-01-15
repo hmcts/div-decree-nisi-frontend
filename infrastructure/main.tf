2469afb0eac408d652fb737d4015525eae35e161
Tim Jacomb <tim.jacomb@hmcts.net>
2019-01-15 09:40:26 +0000

    Use I3 instance size

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -44,6 +44,7 @@ module "frontend" {
   common_tags                     = "${var.common_tags}"
   asp_name                        = "${local.asp_name}"
   asp_rg                          = "${local.asp_rg}"
+  instance_size                   = "I3"
 
   app_settings = {
 
