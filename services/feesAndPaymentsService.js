2a9dc53f540079b777bbb943c8a986d0c5d9c3b6
Mounika <mounika.ammineni@hmcts.net>
2019-01-16 13:37:31 +0000

    url

--- services/feesAndPaymentsService.js
+++ services/feesAndPaymentsService.js
@@ -5,7 +5,7 @@ const logger = require('@hmcts/nodejs-logging').Logger.getLogger(__filename);
 const feeCodeEndpoint = '/fees-and-payments/version/1/';
 
 const get = feeUrl => {
-  const uri = `${config.services.feesAndPayments.baseUrl}${feeCodeEndpoint}${feeUrl}`;
+  const uri = `${config.services.feesAndPayments.url}${feeCodeEndpoint}${feeUrl}`;
   return request.get({ uri, json: true })
     .catch(error => {
       logger.error(`Trying to retrieve fee from fees and payment service: ${error}`);
