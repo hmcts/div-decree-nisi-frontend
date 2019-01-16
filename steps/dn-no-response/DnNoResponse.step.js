f9ed73a650bdef67465978ad7785ab18fe0a711d
Mounika <mounika.ammineni@hmcts.net>
2019-01-16 13:30:53 +0000

    Fees added

--- steps/dn-no-response/DnNoResponse.step.js
+++ steps/dn-no-response/DnNoResponse.step.js
@@ -1,6 +1,7 @@
 const { Page } = require('@hmcts/one-per-page');
 const config = require('config');
 const { stopHere } = require('@hmcts/one-per-page/flow');
+const { getFeeFromFeesAndPayments } = require('middleware/feesAndPaymentsMiddleware');
 
 class DnNoResponse extends Page {
   static get path() {
@@ -13,6 +14,21 @@ class DnNoResponse extends Page {
 
   get flowControl() {
     return stopHere(this);
+  }
+
+  get feeToResendApplication() {
+    return this.res.locals.applicationFee['application-without-notice-fee'].amount;
+  }
+
+  get enforcemetFee() {
+    return this.res.locals.applicationFee['enforcement-fee'].amount;
+  }
+
+  get middleware() {
+    return [
+      getFeeFromFeesAndPayments('application-without-notice-fee'),
+      getFeeFromFeesAndPayments('enforcement-fee')
+    ];
   }
 }
 
