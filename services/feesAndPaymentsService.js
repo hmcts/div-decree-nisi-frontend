edc1599f943fbf76f864945f79d4645d54368059
Mounika <mounika.ammineni@hmcts.net>
2019-01-20 11:03:51 +0000

    testing

--- services/feesAndPaymentsService.js
+++ services/feesAndPaymentsService.js
@@ -1,9 +1,15 @@
 const request = require('request-promise-native');
 const config = require('config');
 
+const logger = require('@hmcts/nodejs-logging').Logger.getLogger(__filename);
+
 const feeCodeEndpoint = '/fees-and-payments/version/1/';
 
 const getFee = feeType => {
+  logger.info({
+    message: 'config loaded',
+    config
+  });
   const uri = `${config.services.feesAndPayments.url}${feeCodeEndpoint}${feeType}`;
   return request.get({ uri, json: true });
 };
