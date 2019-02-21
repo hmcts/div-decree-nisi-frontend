e2d551e1aba215234623eb4604a268b329d4d307
Borislav Petrov <Borislav.Petrov@HMCTS.NET>
2019-02-21 18:21:36 +0000

    redirecting to review co-respondent response step

--- steps/review-aos-response/ReviewAosResponse.step.js
+++ steps/review-aos-response/ReviewAosResponse.step.js
@@ -77,6 +77,14 @@ class ReviewAosResponse extends Question {
     return this.case.respAdmitOrConsentToFact === this.consts.no;
   }
 
+  get adulteryWhishToName() {
+    return this.case.reasonForDivorceAdulteryWishToName && this.case.reasonForDivorceAdulteryWishToName === this.consts.yes;
+  }
+
+  get recievedAosFromCoResp() {
+    return this.case.coRespondentAnswers && this.case.coRespondentAnswers.aos.received === this.consts.yes;
+  }
+
   get responseTemplate() {
     if (this.req.session.case.state === this.consts.viewOnlyState) {
       return this.consts.viewTemplate;
@@ -93,11 +101,17 @@ class ReviewAosResponse extends Question {
       return this.sep2yr && this.doesNotConsent;
     };
 
+    const reviewAosRespCoRespondent = () => {
+      return this.adultery && this.adulteryWhishToName && this.recievedAosFromCoResp && this.exist(this.consts.respAdmitOrConsentToFact);
+    };
+
     return branch(
       redirectTo(this.journey.steps.AmendApplication)
         .if(parseBool(config.features.release520) && ammendAplication),
       redirectTo(this.journey.steps.RespNotAdmitAdultery)
         .if(parseBool(config.features.release520) && respNotAdmitAdultery),
+      redirectTo(this.journey.steps.ReviewAosResponseFromCoRespondent)
+        .if(parseBool(config.features.release520) && reviewAosRespCoRespondent),
       redirectTo(this.journey.steps.ApplyForDecreeNisi)
     );
   }
