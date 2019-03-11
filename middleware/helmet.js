92169280e59cb2c161878b661f65d072c90b8f90
Borislav Petrov <Borislav.Petrov@HMCTS.NET>
2019-03-11 16:57:56 +0000

    helmet unit test fix

--- middleware/helmet.js
+++ middleware/helmet.js
@@ -10,11 +10,11 @@ const setupHelmet = app => {
   app.use(helmet.contentSecurityPolicy({
     directives: {
       fontSrc: ['\'self\' data:'],
-      scriptSrc: ['\'self\'', '\'unsafe-inline\'', 'www.google-analytics.com', 'hmctspiwik.useconnect.co.uk'],
+      scriptSrc: ['\'self\'', '\'unsafe-inline\'', 'www.google-analytics.com', 'hmctspiwik.useconnect.co.uk', 'www.googletagmanager.com'],
       connectSrc: ['\'self\''],
       mediaSrc: ['\'self\''],
       frameSrc: ['\'none\''],
-      imgSrc: ['\'self\'', 'www.google-analytics.com', 'hmctspiwik.useconnect.co.uk', 'www.googletagmanager.com']
+      imgSrc: ['\'self\'', 'www.google-analytics.com', 'hmctspiwik.useconnect.co.uk']
     }
   }));
 
