a1076fc7e91fa8cca84d112e2a720635546e006e
Tony Chow <tony.chow@hmcts.net>
2019-04-01 12:33:18 +0100

    Update Idam Express Middleware version
    Refactored healthcheck unit test

--- test/unit/middleware/healthcheck.test.js
+++ test/unit/middleware/healthcheck.test.js
@@ -63,7 +63,7 @@ describe(modulePath, () => {
       .then(done, done);
   });
 
-  it('throws an error if healthcheck fails for idam-web-app', () => {
+  it('throws an error if healthcheck fails for idam-api', () => {
     setupHealthChecks(app);
 
     const idamCallback = healthcheck.web.firstCall.args[1].callback;
@@ -72,19 +72,10 @@ describe(modulePath, () => {
     sinon.assert.calledWith(logger.error);
   });
 
-  it('throws an error if healthcheck fails for idam-api', () => {
-    setupHealthChecks(app);
-
-    const idamCallback = healthcheck.web.secondCall.args[1].callback;
-    idamCallback('error');
-
-    sinon.assert.calledWith(logger.error);
-  });
-
   it('throws an error if healthcheck fails for case-orchestration-service', () => {
     setupHealthChecks(app);
 
-    const cosCallback = healthcheck.web.thirdCall.args[1].callback;
+    const cosCallback = healthcheck.web.secondCall.args[1].callback;
     cosCallback('error');
 
     sinon.assert.calledWith(logger.error);
@@ -99,7 +90,7 @@ describe(modulePath, () => {
     sinon.assert.called(outputs.up);
   });
 
-  it('throws an error if healthcheck fails for idam-api', () => {
+  it('passes healthcheck for idam-api', () => {
     setupHealthChecks(app);
 
     const idamCallback = healthcheck.web.secondCall.args[1].callback;
