cfc7189990cadf66d83da34f5b234f7843f8295c
Dominic Taylor <dom.taylor7@gmail.com>
2019-04-05 11:23:54 +0100

    correct start redirection

--- steps/start/Start.step.js
+++ steps/start/Start.step.js
@@ -1,6 +1,7 @@
 const { Redirect, branch } = require('@hmcts/one-per-page');
 const { redirectTo } = require('@hmcts/one-per-page/flow');
 const config = require('config');
+const { parseBool } = require('@hmcts/one-per-page/util');
 
 class Start extends Redirect {
   static get path() {
@@ -12,8 +13,10 @@ class Start extends Redirect {
       return this.req.session && this.req.session.case;
     };
 
+    const nextStep = parseBool(config.features.showSystemMessage) ? this.journey.steps.SystemMessage : this.journey.steps.PetitionProgressBar;
+
     return branch(
-      redirectTo(this.journey.steps.PetitionProgressBar).if(hasSession),
+      redirectTo(nextStep).if(hasSession),
       redirectTo(this.journey.steps.Entry)
     );
   }
--- test/unit/steps/start.test.js
+++ test/unit/steps/start.test.js
@@ -3,19 +3,50 @@ const modulePath = 'steps/start/Start.step';
 const Start = require(modulePath);
 const PetitionProgressBar = require('steps/petition-progress-bar/PetitionProgressBar.step');
 const Entry = require('steps/entry/Entry.step');
-const { redirect } = require('@hmcts/one-per-page-test-suite');
+const { redirect, sinon } = require('@hmcts/one-per-page-test-suite');
+const SystemMessage = require('steps/system-message/SystemMessage.step');
+const config = require('config');
 
 describe(modulePath, () => {
-  context('navigation', () => {
-    it('to PetitionProgressBar page', () => {
-      const session = {
-        case: {}
-      };
-      return redirect.navigatesToNext(Start, PetitionProgressBar, session);
-    });
+  const sandbox = sinon.createSandbox();
 
+  context('navigation', () => {
     it('to entry page', () => {
       return redirect.navigatesToNext(Start, Entry);
+    });
+
+    describe('showSystemMessage feature off', () => {
+      before(() => {
+        sandbox.replace(config.features, 'showSystemMessage', false);
+      });
+
+      after(() => {
+        sandbox.restore();
+      });
+
+      it('to PetitionProgressBar page', () => {
+        const session = {
+          case: {}
+        };
+        return redirect.navigatesToNext(Start, PetitionProgressBar, session);
+      });
+    });
+
+    describe('showSystemMessage feature on', () => {
+      before(() => {
+        sandbox.replace(config.features, 'showSystemMessage', true);
+      });
+
+      after(() => {
+        sandbox.restore();
+      });
+
+      it('to SystemMessage page', () => {
+        const session = {
+          case: {}
+        };
+        return redirect.navigatesToNext(Start, SystemMessage, session);
+      });
     });
   });
 });
