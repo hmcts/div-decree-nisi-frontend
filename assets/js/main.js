0d054a92fdae11639f5f4ca6bb9090a9eeca2c88
Dominic Taylor <dom.taylor7@gmail.com>
2019-05-15 14:53:52 +0100

    govuk design updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,15 @@
-import $ from 'jquery';
-import ShowHideContent from 'govuk/show-hide-content';
-import './documentUpload';
+/* eslint-disable */
 
-$(document).ready(() => {
-  const showHideContent = new ShowHideContent();
+import $ from 'jquery';
+import govukFrontend from 'govuk-frontend/all';
+import './documentUpload';
+import './showHideContent';
+
+window.jQuery = $;
+
+$(document).ready(function() {
+  const showHideContent = new global.GOVUK.ShowHideContent();
   showHideContent.init();
 
-  $('input.button[type="submit"]').click(event => {
-    const $el = $(event.target);
-    setTimeout(() => {
-      $el.attr('disabled', true);
-      $el.attr('aria-disabled', true);
-    });
-  });
+  govukFrontend.initAll();
 });
