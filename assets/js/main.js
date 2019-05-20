3d225fefb229a77bd49a8b8cc67a7e9e7b819486
Alex Wakeman <wakemana@gmail.com>
2019-05-20 16:10:02 +0100

    Add jquery hook for on click submit button to disable it

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,12 @@ import './documentUpload';
 $(document).ready(() => {
   const showHideContent = new ShowHideContent();
   showHideContent.init();
+
+  $('input.button[type="submit"]').click(event => {
+    const $el = $(event.target);
+    setTimeout(() => {
+      $el.attr('disabled', true);
+      $el.attr('aria-disabled', true);
+    });
+  });
 });
