41d69affde8a372b2d1a081842c3bb42ba4dcc43
Dominic Taylor <dom.taylor7@gmail.com>
2019-06-10 11:18:57 +0100

    put double click protection back in

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,5 +11,13 @@ $(document).ready(() => {
   const showHideContent = new global.GOVUK.ShowHideContent();
   showHideContent.init();
 
+  $('input.button[type="submit"]').click(event => {
+    const $el = $(event.target);
+    setTimeout(() => {
+      $el.attr('disabled', true);
+      $el.attr('aria-disabled', true);
+    });
+  });
+
   govukFrontend.initAll();
 });
