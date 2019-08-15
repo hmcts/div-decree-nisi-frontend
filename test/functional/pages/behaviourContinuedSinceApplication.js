6cd7833c1d7d86c675aec89636a86b33cc0f3aa7
vianvi <30834324+vianvi@users.noreply.github.com>
2019-08-15 10:58:10 +0100

    fix failed tests

--- test/functional/pages/behaviourContinuedSinceApplication.js
+++ test/functional/pages/behaviourContinuedSinceApplication.js
@@ -11,12 +11,9 @@ const config = require('config');
 function testBehaviourContinuedSinceApplicationPage() {
   const I = this;
 
-  I.waitInUrl(BehaviourContinuedSinceApplication.path, 5);
   I.amOnLoadedPage(BehaviourContinuedSinceApplication.path);
-  if (config.tests.functional.verifyOnCrossbrowser) {
-    I.wait(2);
-  }
-  I.click(BehaviourContinuedSinceApplicationContent.en.fields.changes.behaviourContinuedSinceApplication.yes); // eslint-disable-line
+  I.waitInUrl(BehaviourContinuedSinceApplication.path, 5);
+  I.retry(2).click(BehaviourContinuedSinceApplicationContent.en.fields.changes.behaviourContinuedSinceApplication.yes); // eslint-disable-line
   I.navByClick(commonContent.en.continue);
   I.waitInUrl(ClaimCosts.path, 5);
   I.seeCurrentUrlEquals(ClaimCosts.path);
