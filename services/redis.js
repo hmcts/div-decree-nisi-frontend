7d79ddd0380547163b4183d4a438a29330698f87
Barry Weeks <barry.weeks@justice.gov.uk>
2021-12-08 16:58:40 +0000

    Update redis.js
    Remove additional params from redis.createClient() calls

--- services/redis.js
+++ services/redis.js
@@ -3,9 +3,7 @@ const config = require('config');
 const logger = require('services/logger').getLogger(__filename);
 
 const client = redis.createClient(
-  config.services.redis.url,
-  { enableOfflineQueue: false }
-);
+  config.services.redis.url);
 client.on('error', error => {
   logger.errorWithReq(null, 'redis_error', 'Error connecting to Redis', error);
 });
