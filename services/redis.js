49d5ed6c4d6964242a1fa5e9d92063e6aadecf34
Ruban <ruban72@hotmail.com>
2021-12-15 08:03:23 +0000

    DFR-701: Point to AAT Redis

--- services/redis.js
+++ services/redis.js
@@ -1,11 +1,16 @@
-const ioRedis = require('ioredis');
+const Redis = require('ioredis');
 const config = require('config');
 const logger = require('services/logger').getLogger(__filename);
 
-const client = ioRedis.createClient(
+const client = new Redis(
   config.services.redis.url,
   { enableOfflineQueue: false }
 );
+
+client.on('connect', () => {
+  logger.infoWithReq(null, 'redis_connect', 'Connected to Redis');
+});
+
 client.on('error', error => {
   logger.errorWithReq(null, 'redis_error', 'Error connecting to Redis', error);
 });
