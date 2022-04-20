8c44543f81e6d3328042bc3a67cb6a69584cb0c9
Barry Weeks <barry.weeks@justice.gov.uk>
2022-04-20 11:44:28 +0100

    Add webchat version to app and html
    Add antennaWebchat version to app and html

--- app.js
+++ app.js
@@ -67,7 +67,8 @@ lookAndFeel.configure(app, {
       googleAnalyticsId: config.services.googleAnalytics.id,
       antennaWebchat: {
         url: config.services.antennaWebchat.url,
-        service: config.services.antennaWebchat.service
+        service: config.services.antennaWebchat.service,
+        version: config.services.antennaWebchat.version
       },
       features: {
         antennaWebchat: parseBool(config.features.antennaWebchat),
