7bc3d067f386bdb86904c2f55911d5356c2f4149
KleoG <kleogeorgiou@hotmail.com>
2022-08-31 16:09:26 +0100

    yarn lint fixes

--- test/functional/paths/intergration.js
+++ test/functional/paths/intergration.js
@@ -1,47 +1,47 @@
-//const CheckYourAnswersContent = require('steps/check-your-answers/CheckYourAnswers.content');
-//const config = require('config');
-//const basicDivorceSession = require('test/resources/basic-divorce-session');
+//  const CheckYourAnswersContent = require('steps/check-your-answers/CheckYourAnswers.content');
+//  const config = require('config');
+//  const basicDivorceSession = require('test/resources/basic-divorce-session');
 //
-//const testHappyPath = async(I, language = 'en') => {
-//  await I.retry(2).createAUser();
-//  await I.retry(2).createDnCaseForUser(basicDivorceSession);
+//  const testHappyPath = async(I, language = 'en') => {
+//    await I.retry(2).createAUser();
+//    await I.retry(2).createDnCaseForUser(basicDivorceSession);
 //
-//  I.amOnLoadedPage('/', language);
-//  await I.testIdamPage();
+//    I.amOnLoadedPage('/', language);
+//    await I.testIdamPage();
 //
-//  I.testProgressBar(language);
-//  I.testApplyForDecreeNisiPage(language);
-//  I.testMiniPetitionPage(language);
+//    I.testProgressBar(language);
+//    I.testApplyForDecreeNisiPage(language);
+//    I.testMiniPetitionPage(language);
 //
-//  if (config.tests.functional.verifyOnCrossbrowser) {
-//    I.testBehaviourContinuedSinceApplicationPage(language);
-//  } else {
-//    const isMocked = await I.grabCookie('mockIdamUserDetails');
-//    if (isMocked) {
-//      I.testLivedApartSinceSeparationPage(language);
-//    } else {
+//    if (config.tests.functional.verifyOnCrossbrowser) {
 //      I.testBehaviourContinuedSinceApplicationPage(language);
+//    } else {
+//      const isMocked = await I.grabCookie('mockIdamUserDetails');
+//      if (isMocked) {
+//        I.testLivedApartSinceSeparationPage(language);
+//      } else {
+//        I.testBehaviourContinuedSinceApplicationPage(language);
+//      }
 //    }
-//  }
 //
-//  I.testClaimCostsPage(language);
-//  I.testShareCourtDocumentsPage('no', language);
+//    I.testClaimCostsPage(language);
+//    I.testShareCourtDocumentsPage('no', language);
 //
-//  I.testCheckYourAnswersPage(language);
-//  I.navByClick(CheckYourAnswersContent[language].submit);
-//  if (config.tests.functional.verifyOnCrossbrowser) {
-//    I.waitForText('LV18D81234', 30);
-//  } else {
-//    I.amOnLoadedPage(config.paths.done, language);
-//  }
-//};
+//    I.testCheckYourAnswersPage(language);
+//    I.navByClick(CheckYourAnswersContent[language].submit);
+//    if (config.tests.functional.verifyOnCrossbrowser) {
+//      I.waitForText('LV18D81234', 30);
+//    } else {
+//      I.amOnLoadedPage(config.paths.done, language);
+//    }
+//  };
 //
-//Feature('Integration Tests');
+//  Feature('Integration Tests');
 //
-//Scenario('Happy Path for English language @cross-browser-test', async I => {
-//  await testHappyPath(I, 'en');
-//}).retry(2);
+//  Scenario('Happy Path for English language @cross-browser-test', async I => {
+//    await testHappyPath(I, 'en');
+//  }).retry(2);
 //
-//Scenario('Happy Path for Welsh language', async I => {
-//  await testHappyPath(I, 'cy');
-//}).retry(2);
+//  Scenario('Happy Path for Welsh language', async I => {
+//    await testHappyPath(I, 'cy');
+//  }).retry(2);
--- test/functional/paths/pages.js
+++ test/functional/paths/pages.js
@@ -1,65 +1,65 @@
-//const basicDivorceSession = require('test/resources/basic-divorce-session');
+//  const basicDivorceSession = require('test/resources/basic-divorce-session');
 //
-//const testAllPages = async(I, language = 'en') => {
-//  await I.retry(2).createAUser();
+//  const testAllPages = async(I, language = 'en') => {
+//    await I.retry(2).createAUser();
 //
-//  await I.retry(2).createDnCaseForUser(basicDivorceSession);
+//    await I.retry(2).createDnCaseForUser(basicDivorceSession);
 //
-//  I.amOnLoadedPage('/', language);
+//    I.amOnLoadedPage('/', language);
 //
-//  await I.testIdamPage();
+//    await I.testIdamPage();
 //
-//  I.testProgressBar(language);
+//    I.testProgressBar(language);
 //
-//  I.testDnNoResponse(language);
+//    I.testDnNoResponse(language);
 //
-//  I.testAmendApplication(language);
+//    I.testAmendApplication(language);
 //
-//  I.testRespNotAdmitAdultery(language);
+//    I.testRespNotAdmitAdultery(language);
 //
-//  I.testReviewAosResponseFromCoRespondent(language);
+//    I.testReviewAosResponseFromCoRespondent(language);
 //
-//  I.testAdulteryFirstFound(language);
+//    I.testAdulteryFirstFound(language);
 //
-//  I.testApplyForDecreeNisiPage(language);
+//    I.testApplyForDecreeNisiPage(language);
 //
-//  I.testBehaviourContinuedSinceApplicationPage(language);
+//    I.testBehaviourContinuedSinceApplicationPage(language);
 //
-//  I.testClaimCostsPage(language);
+//    I.testClaimCostsPage(language);
 //
-//  I.testIntolerable(language);
+//    I.testIntolerable(language);
 //
-//  I.testLivedApartSinceAdulteryPage(language);
+//    I.testLivedApartSinceAdulteryPage(language);
 //
-//  I.testLivedApartSinceDesertionPage(language);
+//    I.testLivedApartSinceDesertionPage(language);
 //
-//  I.testLivedApartSinceLastIncidentDatePage(language);
+//    I.testLivedApartSinceLastIncidentDatePage(language);
 //
-//  I.testLivedApartSinceSeparationPage(language);
+//    I.testLivedApartSinceSeparationPage(language);
 //
-//  I.testMiniPetitionPage(language);
+//    I.testMiniPetitionPage(language);
 //
-//  I.testReviewAosResponsePage(language);
+//    I.testReviewAosResponsePage(language);
 //
-//  I.testShareCourtDocumentsPage('yes', language);
+//    I.testShareCourtDocumentsPage('yes', language);
 //
-//  await I.testUploadPage(language);
+//    await I.testUploadPage(language);
 //
-//  I.testDesertionAskedToResumeDN(language);
+//    I.testDesertionAskedToResumeDN(language);
 //
-//  I.testCourtFeedback(language);
+//    I.testCourtFeedback(language);
 //
-//  I.testCheckYourAnswersPage(language);
+//    I.testCheckYourAnswersPage(language);
 //
-//  await I.testDonePage();
-//};
+//    await I.testDonePage();
+//  };
 //
-//Feature('Test all pages');
+//  Feature('Test all pages');
 //
-//Scenario('Pages with English language', async I => {
-//  await testAllPages(I, 'en');
-//}).retry(3);
+//  Scenario('Pages with English language', async I => {
+//    await testAllPages(I, 'en');
+//  }).retry(3);
 //
-//Scenario('Pages with Welsh language', async I => {
-//  await testAllPages(I, 'cy');
-//}).retry(3);
+//  Scenario('Pages with Welsh language', async I => {
+//    await testAllPages(I, 'cy');
+//  }).retry(3);
