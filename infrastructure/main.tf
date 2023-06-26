feed1b1095ad3dd3f8cc0307fdf2b18a7faca760
Mike Lemos <gobii@me.com>
2023-06-26 14:17:06 +0100

    DFR-1199 upgrade to redis 6 (#695)
    * DFR-1199 upgrade to redis 6
    
    * DFR-1199 removing the nvmrc warning
    
    * DFR-1199 removing the nvmrc warning
    
    * DFR-1199 removing the nvmrc warning
    
    * DFR-1999 removing redis to get the one from shared
    
    * Bumping chart version/ fixing aliases
    
    * DFR-1999 removing redis to get the one from shared
    
    ---------
    
    Co-authored-by: hmcts-jenkins-d-to-i <62423932+hmcts-jenkins-d-to-i[bot]@users.noreply.github.com>

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -14,15 +14,6 @@ data "azurerm_subnet" "core_infra_redis_subnet" {
   resource_group_name  = "core-infra-${var.env}"
 }
 
-module "redis-cache" {
-  source   = "git@github.com:hmcts/cnp-module-redis?ref=master"
-  product  = var.env != "preview" ? "${var.product}-redis" : "${var.product}-${var.component}-redis"
-  location = var.location
-  env      = var.env
-  subnetid = data.azurerm_subnet.core_infra_redis_subnet.id
-  common_tags = var.common_tags
-}
-
 data "azurerm_key_vault" "div_key_vault" {
   name                = local.vaultName
   resource_group_name = local.vaultName
@@ -40,11 +31,5 @@ data "azurerm_key_vault_secret" "session_secret" {
 
 data "azurerm_key_vault_secret" "redis_secret" {
   name      = "redis-secret"
-  key_vault_id = data.azurerm_key_vault.div_key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "redis_connection_string" {
-  name = "${var.component}-redis-connection-string"
-  value = "redis://ignore:${urlencode(module.redis-cache.access_key)}@${module.redis-cache.host_name}:${module.redis-cache.redis_port}?tls=true"
   key_vault_id = data.azurerm_key_vault.div_key_vault.id
 }
